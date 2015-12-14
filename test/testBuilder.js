cce1ba2fae1a9c146bd455d52eb10f844b133712
Aubrey Hewes <aubrey@hewes.org.uk>
2015-12-14 20:52:15 +0100

    changed assertion evaluation order to conform to interface

--- test/testBuilder.js
+++ test/testBuilder.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var assert = require("assert");
+
 var builder = require("../src/builder.js");
 
 describe("builder.js", function() {
@@ -11,36 +12,36 @@ describe("builder.js", function() {
     describe("Comparison Query Operators", function () {
 
       it("$eq", function () {
-        assert.equal('`test` = "test"', builder({"test": "test"}));
-        assert.equal('`test` = "test"', builder({"test": {"$eq": "test"}}));
+        assert.equal(builder({"test": "test"}), '`test` = "test"');
+        assert.equal(builder({"test": {"$eq": "test"}}), '`test` = "test"');
       });
 
       it("$gt", function () {
-        assert.equal("`test` > 1", builder({"test": {"$gt": 1}}));
+        assert.equal(builder({"test": {"$gt": 1}}), "`test` > 1");
       });
 
       it("$gte", function () {
-        assert.equal("`test` >= 1", builder({"test": {"$gte": 1}}));
+        assert.equal(builder({"test": {"$gte": 1}}), "`test` >= 1");
       });
 
       it("$lt", function () {
-        assert.equal("`test` < 1", builder({"test": {"$lt": 1}}));
+        assert.equal(builder({"test": {"$lt": 1}}), "`test` < 1");
       });
 
       it("$lte", function () {
-        assert.equal("`test` <= 1", builder({"test": {"$lte": 1}}));
+        assert.equal(builder({"test": {"$lte": 1}}), "`test` <= 1");
       });
 
       it("$ne", function () {
-        assert.equal("`test` != 1", builder({"test": {"$ne": 1}}));
+        assert.equal(builder({"test": {"$ne": 1}}), "`test` != 1");
       });
 
       it("$in", function () {
-        assert.equal("`test` IN (1)", builder({"test": {"$in": [1]}}));
+        assert.equal(builder({"test": {"$in": [1]}}), "`test` IN (1)");
       });
 
       it("$nin", function () {
-        assert.equal("`test` NOT IN (1)", builder({"test": {"$nin": [1]}}));
+        assert.equal(builder({"test": {"$nin": [1]}}), "`test` NOT IN (1)");
       });
 
     });
@@ -48,11 +49,11 @@ describe("builder.js", function() {
     describe("Logical Query Operators", function () {
 
       it("$or", function () {
-        assert.equal("(`col1` = 1 OR `col2` = 2)", builder({"$or": [{"col1": 1}, {"col2": 2}]}));
+        assert.equal(builder({"$or": [{"col1": 1}, {"col2": 2}]}), "(`col1` = 1 OR `col2` = 2)");
       });
 
       it("$and", function () {
-        assert.equal("(`col1` = 1 AND `col2` = 2)", builder({"$and": [{"col1": 1}, {"col2": 2}]}));
+        assert.equal(builder({"$and": [{"col1": 1}, {"col2": 2}]}), "(`col1` = 1 AND `col2` = 2)");
       });
 
       it("$not"); // not yet implemented
@@ -64,10 +65,10 @@ describe("builder.js", function() {
     describe("Evaluation Query Operators", function () {
 
       it("$regex", function () {
-        assert.equal('`test` REGEXP BINARY "value.*"', builder({"test": /value.*/}));
-        assert.equal('`test` REGEXP "value.*"', builder({"test": /value.*/i}));
-        assert.equal('`test` REGEXP BINARY "value.*"', builder({"test": {"$regex": "value.*"}}));
-        assert.equal('`test` REGEXP "value.*"', builder({"test": {"$regex": "value.*", $options: 'i'}}));
+        assert.equal(builder({"test": /value.*/}), '`test` REGEXP BINARY "value.*"');
+        assert.equal(builder({"test": /value.*/i}), '`test` REGEXP "value.*"');
+        assert.equal(builder({"test": {"$regex": "value.*"}}), '`test` REGEXP BINARY "value.*"');
+        assert.equal(builder({"test": {"$regex": "value.*", $options: 'i'}}), '`test` REGEXP "value.*"');
       });
 
     });
@@ -78,22 +79,22 @@ describe("builder.js", function() {
 
     it("single comparison and single logical $and", function () {
       assert.equal(
-        "(`col1` = 1 AND (`col2` = 2 AND `col3` = 3))",
-        builder({'col1': 1, "$and": [{"col2": 2}, {"col3": 3}]})
+        builder({'col1': 1, "$and": [{"col2": 2}, {"col3": 3}]}),
+        "(`col1` = 1 AND (`col2` = 2 AND `col3` = 3))"
       );
     });
 
     it("multiple comparison and single logical $and", function () {
       assert.equal(
-        "(`col1` = 1 AND `col2` = 2 AND (`col3` = 3 AND `col4` = 4))",
-        builder({'col1': 1, 'col2': 2, "$and": [{"col3": 3}, {"col4": 4}]})
+        builder({'col1': 1, 'col2': 2, "$and": [{"col3": 3}, {"col4": 4}]}),
+        "(`col1` = 1 AND `col2` = 2 AND (`col3` = 3 AND `col4` = 4))"
       );
     });
 
     it("multiple comparison and single logical $and and single logical $or", function () {
       assert.equal(
-        "(`col1` = 1 AND `col2` = 2 AND (`col3` = 3 AND `col4` = 4) AND (`col5` = 5 OR `col6` = 6))",
-        builder({'col1': 1, 'col2': 2, "$and": [{"col3": 3}, {"col4": 4}], "$or": [{"col5": 5}, {"col6": 6}]})
+        builder({'col1': 1, 'col2': 2, "$and": [{"col3": 3}, {"col4": 4}], "$or": [{"col5": 5}, {"col6": 6}]}),
+        "(`col1` = 1 AND `col2` = 2 AND (`col3` = 3 AND `col4` = 4) AND (`col5` = 5 OR `col6` = 6))"
       );
     });
 
@@ -103,17 +104,6 @@ describe("builder.js", function() {
 
     it("extreme nesting", function () {
       assert.equal(
-        /*jshint -W101*/
-        /*jscs:disable maximumLineLength*/
-        "(" +
-        "`col1` = 1 " +
-        "AND (" +
-        "(`col2` = 2 OR `col3` = 3) AND `col4` = 4 AND `col5` != 5 AND (`col6` > 6 AND `col7` <= 7 AND (`col8` > 6 OR `col9` <= 7))) " +
-        "AND (" +
-        "(`col2` = 2 OR `col3` = 3) OR `col4` = 4 OR `col5` != 5 OR (`col6` > 6 AND `col7` <= 7 AND (`col8` > 6 OR `col9` <= 7)))" +
-        ")",
-        /*jscs:enable maximumLineLength*/
-       /*jshint +W101*/
         builder({
           'col1': 1,
           "$and": [
@@ -172,7 +162,18 @@ describe("builder.js", function() {
               ]
             }
           ],
-        })
+        }),
+        /*jshint -W101*/
+        /*jscs:disable maximumLineLength*/
+        "(" +
+        "`col1` = 1 " +
+        "AND (" +
+        "(`col2` = 2 OR `col3` = 3) AND `col4` = 4 AND `col5` != 5 AND (`col6` > 6 AND `col7` <= 7 AND (`col8` > 6 OR `col9` <= 7))) " +
+        "AND (" +
+        "(`col2` = 2 OR `col3` = 3) OR `col4` = 4 OR `col5` != 5 OR (`col6` > 6 AND `col7` <= 7 AND (`col8` > 6 OR `col9` <= 7)))" +
+        ")"
+        /*jscs:enable maximumLineLength*/
+       /*jshint +W101*/
       );
     });
 
@@ -181,15 +182,15 @@ describe("builder.js", function() {
   describe("Value Casting", function () {
 
     it("should return integer", function () {
-      assert.equal("`test` = 1", builder({"test": 1}));
+      assert.equal(builder({"test": 1}), "`test` = 1");
     });
 
     it("should return is_null", function () {
-      assert.equal("`test` IS NULL", builder({"test": null}));
+      assert.equal(builder({"test": null}), "`test` IS NULL");
     });
 
     it("should return boolean", function () {
-      assert.equal("`test` = true", builder({"test": true}));
+      assert.equal(builder({"test": true}), "`test` = true");
     });
 
   });
